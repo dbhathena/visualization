fdc9c9f59d5bc7c776d413d5292f39994248a9ec
Darian Bhathena <darianb@mit.edu>
2018-10-02 23:04:03 -0400

    configure settings

--- src/viz_platform/settings.py
+++ src/viz_platform/settings.py
@@ -75,12 +75,23 @@ WSGI_APPLICATION = 'viz_platform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'viz-db',
+#         'USER': 'deploy',
+#         'PASSWORD': 'mghmit1',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': passwords.MYSQL_DB,
-        'USER': passwords.MYSQL_USER,
-        'PASSWORD': passwords.MYSQL_PASSWORD,
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'viz-db',
+        'USER': 'deploy',
+        'PASSWORD': 'mghmit1',
         'HOST': '',
         'PORT': '',
     }

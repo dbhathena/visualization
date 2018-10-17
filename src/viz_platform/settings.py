acd493b2db29b64c6fe813306f84cb92a5e78b8a
Darian Bhathena <darianb@mit.edu>
2018-10-17 03:50:15 -0400

    Replace host settings

--- src/viz_platform/settings.py
+++ src/viz_platform/settings.py
@@ -75,27 +75,27 @@ WSGI_APPLICATION = 'viz_platform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'viz_db',
-#         'USER': 'deploy',
-#         'PASSWORD': 'mghmit1',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'viz_db',
         'USER': 'deploy',
         'PASSWORD': 'mghmit1',
-        'HOST': '',
-        'PORT': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'viz_db',
+#         'USER': 'deploy',
+#         'PASSWORD': 'mghmit1',
+#         'HOST': '',
+#         'PORT': '',
+#     }
+# }
 
 
 # Password validation

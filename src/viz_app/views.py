98e1bfdd78129534d4ed9f9c7155cbd1702e67e7
Darian Bhathena <darianb@mit.edu>
2018-11-03 16:51:13 -0400

    Format login page

--- src/viz_app/views.py
+++ src/viz_app/views.py
@@ -79,8 +79,8 @@ def faq(request):
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label='Username')
-    password = forms.CharField(widget=forms.PasswordInput, label='Password')
+    username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'class': 'form-input', 'id': 'id_username'}))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-input', 'id': 'id_password'}))
 
 
 def login(request):

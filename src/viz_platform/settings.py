ae7e10681156e60afc51a436ded7e2702f9004a2
Darian Bhathena <darianb@mit.edu>
2018-11-09 02:11:23 -0500

    Try setting session expire at browser close

--- src/viz_platform/settings.py
+++ src/viz_platform/settings.py
@@ -71,6 +71,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'viz_platform.wsgi.application'
 
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases

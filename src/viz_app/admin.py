440db78d804fe0bb12fdcf46023f076f11638d4a
Darian Bhathena <darianb@mit.edu>
2019-03-29 17:16:06 -0400

    Allow admin to add demographic data

--- src/viz_app/admin.py
+++ src/viz_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import PhysData
+from .models import DemographicData
 
 # Register your models here.
-admin.site.register(PhysData)
+admin.site.register(DemographicData)

dc406bac5e8885be32926fee1d1414ad693a1e33
Noah Faro <nfaro@mit.edu>
2021-02-03 02:01:27 -0500

    Updates to the Website.

--- src/static/js/photo_gallery.js
+++ src/static/js/photo_gallery.js
@@ -9,7 +9,9 @@ function showSlides() {
     slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 4000); // Change image every 2 seconds
 }

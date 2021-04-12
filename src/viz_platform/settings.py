fbff9ac0d19f3eed640f9587ea20b5e4e9558cd5
nfaro <nfaro@mit.edu>
2021-04-12 15:09:10 -0400

    Updating with New Users and to new Version of Django.

--- src/viz_platform/settings.py
+++ src/viz_platform/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user_visit',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'user_visit.middleware.UserVisitMiddleware',
 ]
 
 ROOT_URLCONF = 'viz_platform.urls'
